869c189a46d0a16c68969c53aad7bf70de2ae68e
cBeaur <cedrik.beauregard@boreas.ca>
2025-02-27 16:44:49 -0500

    SQL + users routes

--- app/main.py
+++ app/main.py
@@ -1,17 +1,27 @@
-from typing import Optional
-from fastapi import Body, FastAPI, Response, status, HTTPException
-from pydantic import BaseModel
-from random import randrange
+from fastapi import FastAPI
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from . import models
+from .database import engine
+from . routers import post, user
 
+
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
-
+while True:
+    try:
+        conn = psycopg2.connect(
+            host='localhost', dbname='fastapi', user='postgres', password='1234', cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print('Connected to the database successfully.')
+        break
+    except Exception as error:
+        print('Connection to the database failed.')
+        print("Error: ", error)
+        time.sleep(2)
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
             {"title": "favorite foods", "content": "I like pizza", "id": 2}]
@@ -24,49 +34,10 @@ def find_post(id: int):
     return None
 
 
+app.include_router(post.router)
+app.include_router(user.router)
+
+
 @app.get("/")
 async def root():
-
     return {"message": "Hello World"}
-
-
-@app.get("/posts")
-def get_posts():
-    return {"data": my_posts}
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(post: Post):
-    post_dict = post.model_dump()
-    post_dict["id"] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
-
-
-@app.get("/posts/{id}")
-def get_post(id: int):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id: {id} was not found")
-    return {"post_detail": post}
-
-
-@app.delete("/posts/{id}")
-def delete_post(id: int, status_code=status.HTTP_204_NO_CONTENT):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id: {id} was not found")
-    my_posts.remove(post)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    post_to_update = find_post(id)
-    if not post_to_update:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Post with id: {id} was not found")
-    post_to_update.update(post)
-    return {"data": post}
